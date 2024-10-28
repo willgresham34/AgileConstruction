da1d442cd1eedc5e89aafffa0230e8f96554acbb
Will Gresham <willgresham34@gmail.com>
2024-10-28 01:06:54 -0400

    set up

---
+++ agile-app/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<nav class="mb-2 flex flex-row items-center justify-between bg-primary p-2 text-white">
+	<div class="flex flex-row">
+		<img src="/Agile_Logo.JPEG" alt="Logo" height="auto" width="60px" />
+		<h1>Agile Construction</h1>
+	</div>
+
+	<ul class="justfity-between flex pr-4">
+		<li><a href="/">Home</a></li>
+		<li>|</li>
+		<li><a href="/about">About</a></li>
+		<li>|</li>
+		<li><a href="/contact">Contact</a></li>
+	</ul>
+</nav>
+
+{@render children()}
+
+<style>
+	li {
+		font-size: larger;
+		padding: 0rem 0.5rem;
+	}
+</style>
---
+++ agile-app/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import * as Carousel from '$lib/components/ui/carousel/index.js';
+	import Autoplay from 'embla-carousel-autoplay';
+
+	const plugin = Autoplay({ delay: 5000, stopOnInteraction: true });
+</script>
+
+<div class=" w-full border p-4">
+	<Carousel.Root plugins={[plugin]}>
+		<Carousel.Content>
+			<Carousel.Item class="basis-1/3">Item 1</Carousel.Item>
+			<Carousel.Item class="basis-1/3">Item 2</Carousel.Item>
+			<Carousel.Item class="basis-1/3">Item 3</Carousel.Item>
+			<Carousel.Item class="basis-1/3">Item 4</Carousel.Item>
+			<Carousel.Item class="basis-1/3">Item 5</Carousel.Item>
+		</Carousel.Content>
+	</Carousel.Root>
+</div>
